7f0dbbae58c4c2bd92f4e0ed1a78e3cdf8d026f4
Robinson7070 <robinsonvictorchukwudi@gmail.com>
2023-03-30 15:55:51 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,48 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_number - prints number with _putchar
- * @n: input number
- * Return: returns void
- */
+  * print_number - prints number with _putchar
+  * @n: input number
+  * Return: returns void
+ **/
 void print_number(int n)
 {
-	int i, j, digit, digits, power;
-	unsigned int temp, numchar, number;
+	unsigned int number;
 
-	digit = 0;
+	number = n;
 	if (n < 0)
 	{
-		_putchar('_');
-		temp = -n;
+	_putchar('-');
+	number = -n;
 	}
-	else
+	if (number / 10 != 0)
 	{
-		temp = n;
+	print_number(number / 10);
 	}
-
-
-	number = temp;
-
-	while (number >= 10)
-	{
-		number = number / 10;
-		digit++;
-	}
-	digits = digit + 1;
-	power = 1;
-	i = 1;
-
-	while (i < digits)
-	{
-		power = power * 10;
-		i++;
-	}
-	j = power;
-	while (j >= 1)
-	{
-		numchar = (temp / j) % 10;
-		_putchar(numchar + '0');
-		j = j / 10;
-	}
+	_putchar((number % 10) + '0');
 }
