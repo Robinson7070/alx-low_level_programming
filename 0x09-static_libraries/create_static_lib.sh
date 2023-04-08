bbb7d52c947f7ffb93dc6b27958251c5dada8bc1
Robinson7070 <robinsonvictorchukwudi@gmail.com>
2023-04-08 06:55:20 -0700

    staticlib

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-gcc -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+LOC=$(pwd);
+
+for i in $(ls $LOC | grep '.`*\.c')
+do
+	objname=$(echo $i | grep -Po '^[[:graph:]]+(?=\.c$)').o
+	gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c $LOC/$i -o $LOC/$objname
+	ar -rc liball.a *.o
+done
